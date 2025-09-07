6d64610471675ccb234be44c2598fed57d3e9f63
Haimonmon <beankor8@gmail.com>
2025-09-07 19:44:35 +0800

    Added: api tested

--- Scrappy/main.py
+++ Scrappy/main.py
@@ -1,20 +1,46 @@
-import scrapers
-import precautions
+import requests
+import subprocess
 
-def scrape(url: str, num: int = 1) -> str:
+from typing import Literal, Dict, Any, List
+
+from .file import FileManager
+
+file_manager = FileManager()
+process: subprocess.Popen = None
+
+def activate() -> None:
+    """ Activates Scrappy port in order to scrape on browser. """
+    global process
+    process = subprocess.Popen(["node", "Scrappy/index.js"])
+
+
+def deactivate() -> None:
+    """ DeActivates or Closing Scrappy port connection. """
+    print("\n [ Scrappy 🎉 ] \n Shutting Down . . . 🍒")
+    process.terminate()
+
+
+def scrape(website: Literal["philstar", "abscbn", "gmanetwork"] = "philstar", limit: int = 1, url_to_save: str = None) -> str:
     """ Scrape a specific news on the givin url site """
-    pass
+    if website not in ["philstar", "abscbn", "gmanetwork"]:
+        print(" Cant scrape that site buddy ☝️🤓.")
+        return
+    
+    # * ACTIVATE SCRAPPY
+    activate()
+    website = website.lower()
 
+    scraped_data: Dict[str, Any] = requests.get(f"http://localhost:3000/scrappy/scrape/{website}")
+    data: List[Dict[str, str]] = scraped_data.json()
 
-def add_dummy(username: str, password: str) -> None:
-    """ Adds an account to the given site if it needed an account """
-    pass
+    if not data["success"]:
+        return data["message"]
 
-
-def add_agent(user_agent: str) -> None:
-    """ Add user agent for precautions """
-    pass
+    deactivate()
+    return data["data"]
 
 
 if __name__ == "__main__":
-      pass
+      scraper = scrape("gmanetwork")
+
+      print(scraper)
