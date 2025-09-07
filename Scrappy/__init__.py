3c6d4099bba12d0367f819da833c2b493a298e8a
Haimonmon <beankor8@gmail.com>
2025-09-07 19:44:04 +0800

    Added: main files

--- Scrappy/__init__.py
+++ Scrappy/__init__.py
@@ -5,8 +5,8 @@ CAUTION:
 Anu action of scraping may cause a ban to the specific website so be careful.
 """
 
-from .main import scrape, add_dummy, add_agent
+from .main import scrape
 
 __all__ = [
-    "scrape", "add_dummy", "add_agent"
+    "scrape"
 ]
