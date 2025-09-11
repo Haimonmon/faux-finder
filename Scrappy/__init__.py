d1b83879c7a360125778ddc029c963fef860eec5
Haimonmon <beankor8@gmail.com>
2025-09-11 23:51:37 +0800

    Added: check up function available use

--- Scrappy/__init__.py
+++ Scrappy/__init__.py
@@ -5,8 +5,8 @@ CAUTION:
 Anu action of scraping may cause a ban to the specific website so be careful.
 """
 
-from .main import scrape
+from .main import scrape, check_up
 
 __all__ = [
-    "scrape"
+    "scrape", "check_up"
 ]
