91e41887cd5d43a4a4e63b8b0d55a88680216e2e
Haimonmon <beankor8@gmail.com>
2025-09-11 23:52:29 +0800

    Modified: working on it

--- Scrappy/controllers/scrapers/gmanetwork.js
+++ Scrappy/controllers/scrapers/gmanetwork.js
@@ -1,6 +1,12 @@
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+puppeteer.use(StealthPlugin());
+
 class GMANetwork {
     constructor() {
-        this.target_link = "https://www.gmanetwork.com/";
+        this.targetLink = "https://www.gmanetwork.com/news/";
+        this.helper = new GMANetworkHelper();
     }
 
 
@@ -9,9 +15,51 @@ class GMANetwork {
      * @param {*} limit
      * @returns {void}
      */
-    scrape(limit) {
-        let data = "Working 1"
-        return data
+    async scrape(limit) {
+        const browser = await puppeteer.launch({ headless: true});
+        const page = await browser.newPage();
+
+        await page.goto(this.targetLink, {
+            waitUntil: "domcontentloaded"
+        });
+
+        const justInLinks = this.helper.getJustInLinks(page, limit)
+
+        return justInLinks
+    }
+}
+
+
+class GMANetworkHelper {
+    constructor() {
+
+    }
+
+    /**
+     * 
+     * @param {*} page - browser page.
+     * @param {*} limit - limit how many items you want to get on that browser page open.
+     * @returns {object} - returns an object that have article link, title, published date or time
+     */
+    async getJustInLinks(page, limit) {
+        const dataLinks = await page.$$eval(".just-in-content a", (anchors, limit) => {
+            let results = [];
+            for (let i = 0; i < anchors.length && i < limit; i++) {
+                const newsArticleLink = anchors[i];
+                const newsTitle = newsArticleLink.querySelector(".just-in-story-title");
+                const newsDatePublished = newsArticleLink.querySelector(".just-in-date-published");
+                
+                results.push({
+                    "newsArticleLink": newsArticleLink,
+                    "newsDateTimePublished": newsDatePublished ? newsDatePublished.innerText.trim() : null,
+                    "newsTitle": newsTitle ? newsTitle.innerText.trim() : null
+                });
+            }
+
+            return results
+        }, limit);
+
+        return dataLinks;
     }
 }
 
@@ -27,18 +75,18 @@ class GMANetworkController {
      * @param {*} res 
      * 
      */
-    news(req, res) {
+    async news(req, res) {
         // * Default of 2 news data to be scraped
         const limit = req.query.limit ? parseInt(req.query.limit) : 2;
 
-        let scraped_data = this.target.scrape(limit)
+        let scrapedData = await this.target.scrape(limit)
 
-        if (scraped_data) {
+        if (scrapedData) {
              res.json({
                 "success": true,
-                "source": this.target.target_link,
+                "source": this.target.targetLink,
                 "message": "Succesfully Scraped Data on GMA Network 🍺🤓☝️✨",
-                "data": scraped_data
+                "data": scrapedData
             })
         }
     }
