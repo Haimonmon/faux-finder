4a0641da726260c103fb242dc3c40fb238d44319
Haimonmon <beankor8@gmail.com>
2025-09-13 00:47:51 +0800

    Added: topstories scraping done

--- Scrappy/controllers/scrapers/gmanetwork.js
+++ Scrappy/controllers/scrapers/gmanetwork.js
@@ -1,17 +1,22 @@
+import { franc } from "franc";
+
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+
+import { randomUserAgent, randomDelay } from "../../precautions/antiDetection.js";
 
 puppeteer.use(StealthPlugin());
 
 class GMANetwork {
     constructor() {
-        this.targetLink = "https://www.gmanetwork.com/news/";
+        this.targetLink = "https://www.gmanetwork.com/news/topstories/";
         this.helper = new GMANetworkHelper();
     }
 
 
     /**
-     * 
+     * Yoinks given targeted website contents.
      * @param {*} limit
      * @returns {void}
      */
@@ -19,13 +24,19 @@ class GMANetwork {
         const browser = await puppeteer.launch({ headless: true});
         const page = await browser.newPage();
 
+        await page.setUserAgent(randomUserAgent());
+
         await page.goto(this.targetLink, {
             waitUntil: "domcontentloaded"
         });
 
-        const justInLinks = this.helper.getJustInLinks(page, limit)
+        const justInLinks = await this.helper.lootLatestSection(page, limit)
 
-        return justInLinks
+        // console.log(justInLinks)
+
+        const data = await this.helper.lootArticle(browser, justInLinks)
+
+        return data
     }
 }
 
@@ -36,22 +47,22 @@ class GMANetworkHelper {
     }
 
     /**
-     * 
+     * just gets article links first
      * @param {*} page - browser page.
      * @param {*} limit - limit how many items you want to get on that browser page open.
      * @returns {object} - returns an object that have article link, title, published date or time
      */
-    async getJustInLinks(page, limit) {
-        const dataLinks = await page.$$eval(".just-in-content a", (anchors, limit) => {
+    async lootLatestSection(page, limit) {
+        await page.waitForSelector("#latest_section_stories_content_news a");
+
+        const dataLinks = await page.$$eval("#latest_section_stories_content_news a", (anchors, limit) => {
             let results = [];
             for (let i = 0; i < anchors.length && i < limit; i++) {
-                const newsArticleLink = anchors[i];
-                const newsTitle = newsArticleLink.querySelector(".just-in-story-title");
-                const newsDatePublished = newsArticleLink.querySelector(".just-in-date-published");
-                
+                const newsArticleLink = anchors[i].href;
+                const newsTitle = anchors[i].querySelector(".title");
+
                 results.push({
                     "newsArticleLink": newsArticleLink,
-                    "newsDateTimePublished": newsDatePublished ? newsDatePublished.innerText.trim() : null,
                     "newsTitle": newsTitle ? newsTitle.innerText.trim() : null
                 });
             }
@@ -60,6 +71,74 @@ class GMANetworkHelper {
         }, limit);
 
         return dataLinks;
+    }
+
+    /** 
+     * Scrape multiple articles.
+     * @param {*} browser - Puppeteer browser instance
+     * @param {*} data - contains news article links and news title
+     */
+    async lootArticle(browser, data) {
+        const results = []
+
+        const tasks = data.map((article) => (async () => {
+            await randomDelay(1000, article.length);
+
+            const page = await browser.newPage();
+
+            await page.setUserAgent(randomUserAgent());
+
+            await page.goto(article.newsArticleLink, {
+                waitUntil: "domcontentloaded"
+            });
+
+            await page.waitForSelector("article");
+
+            const articleData = await page.evaluate(() => {
+                const subhead = document.querySelector("p.subhead.metadata")?.textContent.trim() || null;
+                const author =  document.querySelector(".main-byline div div")?.textContent.trim() || document.querySelector(".article-author")?.textContent.trim() || null;
+                const published = document.querySelector("time[itemprop='datePublished']")?.getAttribute("datetime") || document.querySelector(".article-date")?.textContent.trim() || null;
+
+                const date = new Date(published);
+                
+                const formattedPublished = date.toLocaleString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    hour: "numeric",
+                    minute: "2-digit",
+                    hour12: true
+                })
+
+                const articleBody = document.querySelectorAll(".article-body p");
+                const storyMain = document.querySelectorAll(".story_main p");
+
+                const paragraphs = Array.from(articleBody.length ? articleBody : storyMain).map(paragraph => paragraph.innerText.trim()).filter(Boolean);
+
+                return {
+                    subhead, author, publishedDate: formattedPublished, content: paragraphs.join("\n\n")
+                }
+            });
+
+            const language = franc(articleData.content || "");
+
+            if (language == "eng") {
+                results.push({
+                    url: article.newsArticleLink,
+                    headline: article.newsTitle,
+                    subheadline: articleData.subhead,
+                    author: articleData.author,
+                    published: articleData.publishedDate,
+                    contentt: articleData.content
+                })
+            }
+        
+            await page.close();
+        })());
+
+        await Promise.all(tasks);
+
+        return results;
     }
 }
 
