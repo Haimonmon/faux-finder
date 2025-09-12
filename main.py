e9d909733540b736aca36d89780d97084b994ece
Haimonmon <beankor8@gmail.com>
2025-09-13 02:02:39 +0800

    Test: activation of scrappy and csv file saving

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import Scrappy as yoink
+import Scrappy as scrap
 
-# print(yoink.check_up())
+scrap.activate()
 
-scraped_data = yoink.scrape('gmanetwork', limit = 7)
+print(scrap.check_up())
+
+scraped_data = scrap.scrape('gmanetwork', limit=7, url_to_save="data/news1.csv")
+print(scraped_data["message"])
+
+scrap.deactivate()
