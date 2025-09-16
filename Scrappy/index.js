0ae130b38339b7f75a0d8e404a132351900a34fe
Haimonmon <beankor8@gmail.com>
2025-09-16 23:15:23 +0800

    Added: env variables

--- Scrappy/index.js
+++ Scrappy/index.js
@@ -1,10 +1,12 @@
+import 'dotenv/config.js';
+
 import express from 'express';
 
 import scrapers from "./routes/scrapers/index.js"
 
 const scrappy = express();
-const port = 3000;
+const port = process.env.SCRAPPY_PORT || 3000;
 
 scrappy.use("/scrappy", scrapers)
 
-scrappy.listen(port);
+scrappy.listen(port, "0.0.0.0");
