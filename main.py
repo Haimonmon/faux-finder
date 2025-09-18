8f53ecf8c1c48125473fc295e6c00af1132b4b09
Haimonmon <beankor8@gmail.com>
2025-09-19 06:21:10 +0800

    Tested: new gma function

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import Scrappy as scrap
 
 scrap.activate()
 
-print(scrap.check_up())
+# print(scrap.check_up())
 
-scraped_data = scrap.scrape(website = 'gmanetwork', limit = 7, url_to_save = "data/news2.csv")
+scraped_data = scrap.scrape_gmanetwork(url_to_save = "data/news1.csv")
 print(scraped_data["message"])
 
 scrap.deactivate()
